8f4be924b3b8d2d0979899c04322ac5f51c51328
Linnea Larsson <philar-9@student.ltu.se>
2025-11-28 13:08:14 +0100

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module se.ltu.restwebservices {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.sql;
-	requires jakarta.ws.rs;
-    //kommentarade bort nedan rad för fick ett fel som var p.g.a. den, men låter det vara kvar ändå utifall någon inser att det behövs
-    //requires se.ltu.restwebservices;
+    requires jakarta.jakartaee.api;
+    requires java.logging;
+    requires java.sql;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
 
-
-    opens se.ltu.restwebservices to javafx.fxml;
-	exports se.ltu.restwebservices;
-	exports client;
+    exports se.ltu.restwebservices to javafx.graphics;
+    exports data_objects.canvas;
+    exports client to javafx.fxml;
 	opens client to javafx.fxml;
 }
