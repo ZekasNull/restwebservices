e94e395837ae8244e8f1ee7106e3ae35e6b0e890
Linnea Larsson <philar-9@student.ltu.se>
2025-11-28 13:09:31 +0100

    Implemented new CanvasResource endpoints
    Stödjer nu att hämta kursuppgifter och studenter.

--- src/main/java/webservices/CanvasResource.java
+++ src/main/java/webservices/CanvasResource.java
@@ -1,15 +1,18 @@
 package webservices;
 
+import data_objects.canvas.Canvas_Kursuppgift;
+import data_objects.canvas.Canvas_Student;
 import data_objects.canvas.Canvas_StudentBetygDTO;
 import database.DatabaseConnector;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Path("canvas")
 public class CanvasResource {
-
+	private final Logger LOG = Logger.getLogger(CanvasResource.class.getName());
 	private final static DatabaseConnector db = new DatabaseConnector();
 
 	//denna borde ta fram en lista med anändare, namn, kurskod, kursnamn, uppgiftsnummer, uppgiftsnamn och betyg
@@ -24,4 +27,24 @@ public class CanvasResource {
 
 		return db.getCanvasGrades(kurskod, uppgift);
 	}
+
+	@GET
+	@Path("kursuppgifter")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Canvas_Kursuppgift> getKursuppgifterFromCanvas(String kurskod)
+	{
+		LOG.info("getKursuppgifterFromCanvas() called");
+		return db.getKursuppgifterFromCanvas(kurskod);
+	}
+
+
+	@GET
+	@Path("studenter")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Canvas_Student> getCanvasStudents()
+	{
+		return db.getCanvasStudents();
+	}
+
+
 }
