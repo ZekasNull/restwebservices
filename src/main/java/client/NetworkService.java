7878cb179de477b03607d27fd1de692a26991f68
Linnea Larsson <philar-9@student.ltu.se>
2025-11-28 09:30:10 +0100

    Sebastians discord-commit

--- src/main/java/client/NetworkService.java
+++ src/main/java/client/NetworkService.java
@@ -10,6 +10,7 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -22,18 +23,21 @@ import java.util.logging.Logger;
  */
 public class NetworkService {
     private static final Logger LOG = Logger.getLogger(NetworkService.class.getName());
+    private final Client client;
+    private final WebTarget api;
     private DatabaseConnector db = new DatabaseConnector(); //TODO endast temporärt, ska använda http-klienter
     private String baseUrl;
 
     //region constructors
     public NetworkService()
     {
-
+        this("http://localhost:8080/restwebservices"); // ändra om ni har annan bas-url
     }
 
     public NetworkService(String baseUrl)
     {
-        this.baseUrl = baseUrl;
+        this.client = ClientBuilder.newClient();
+        this.api = client.target(baseUrl);
     }
     //endregion
 
@@ -45,54 +49,126 @@ public class NetworkService {
      * @return True om det lyckades, annars false
      * //FIXME should return Response
      */
-    public DatabaseResponse sendLadokResultat(Ladok_Resultat resultat, String studentanvändare)
+    // --- EXEMPEL: Skicka resultat till Ladok (POST) ---
+    public boolean sendLadokResultat(Ladok_Resultat resultat, String studentanvändare)
     {
-        Studentits_Student student = db.getStudentFromStudentits(studentanvändare);
-        resultat.setPersonnummer(student.getPersonnummer());
+        try
+        {
+            // 1) hämta student från StudentITS
+            Studentits_Student student = api
+                    .path("studentits")
+                    .path("students")
+                    .path(studentanvändare)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(Studentits_Student.class);
 
-        return db.InsertNewLadokResult(resultat);
-        //TODO proper HTTP call to ladok endpoint
-		/*
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(baseUrl + "/restwebservices/ladok/regresultat");
+            if (student == null || student.getPersonnummer() == null)
+            {
+                return false;
+            }
 
-		Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
-				.post(Entity.entity(resultat, MediaType.APPLICATION_JSON));
+            resultat.setPersonnummer(student.getPersonnummer());
 
-		return response;
-		 */
+            // 2) skicka resultatet vidare till Ladok
+            Response response = api
+                    .path("ladok")
+                    .path("regresultat")
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .post(Entity.entity(resultat, MediaType.APPLICATION_JSON_TYPE));
+
+            boolean ok = response.getStatus() >= 200 && response.getStatus() < 300;
+            response.close();
+            return ok;
+
+        } catch (Exception e)
+        {
+            return false;
+        }
     }
 
+    // --- EXEMPEL: Hämta Canvas-betyg (GET) ---
     public List<Canvas_StudentBetygDTO> getStudentsAndGrades(String kurskod, int kursuppgift)
     {
-
-        return db.getCanvasGrades(kurskod, kursuppgift);
-        //TODO proper HTTP call to canvas ws endpoint
+        try
+        {
+            return api.path("canvas")
+                    .path("betyg")
+                    .queryParam("kurskod", kurskod)
+                    .queryParam("uppgiftNr", kursuppgift)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(new GenericType<List<Canvas_StudentBetygDTO>>() {
+                    });
+        } catch (Exception e)
+        {
+            return List.of();
+        }
     }
 
     //FIXME egentligen studerar inte alla studenter alla kurser
+    // --- EXEMPEL: Hämta Canvas-studenter (GET) ---
     public List<Canvas_Student> getCanvasStudents()
     {
-        //TODO som HTTP
-        return db.getCanvasStudents();
+        try
+        {
+            return api.path("canvas")
+                    .path("studenter")
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(new GenericType<List<Canvas_Student>>() {
+                    });
+        } catch (Exception e)
+        {
+            return List.of();
+        }
     }
 
+    // --- EXEMPEL: Hämta en students betyg (GET) ---
     public Canvas_Betyg getCanvasGrade(String användare, int uppgift_nr)
     {
-        //TODO ska ske med http som vanligt
-        return db.getCanvasGrade(användare, uppgift_nr);
+        try
+        {
+            return api.path("canvas")
+                    .path("betyg")
+                    .path(användare)
+                    .path(String.valueOf(uppgift_nr))
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(Canvas_Betyg.class);
+        } catch (Exception e)
+        {
+            return null;
+        }
     }
 
+    // --- EXEMPEL: Hämta kurskoder (GET) ---
     public List<String> getAllKurskod()
     {
-        //TODO http, you guessed it
-        return db.getKurskodFromEpok();
+        try
+        {
+            return api.path("epok")
+                    .path("kurser")
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(new GenericType<List<String>>() {
+                    });
+        } catch (Exception e)
+        {
+            return List.of();
+        }
     }
 
+    // --- EXEMPEL: Hämta kursuppgifter (GET) ---
     public List<Canvas_Kursuppgift> getAllKursuppgift(String kurskod)
     {
-        //TODO the same as before
-        return db.getKursuppgifterFromCanvas(kurskod);
+        try
+        {
+            return api.path("canvas")
+                    .path("kursuppgifter")
+                    .queryParam("kurskod", kurskod)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(new GenericType<List<Canvas_Kursuppgift>>() {
+                    });
+        } catch (Exception e)
+        {
+            return List.of();
+        }
     }
 
     /**
